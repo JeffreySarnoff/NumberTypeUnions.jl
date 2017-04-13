2c6e8c52473c3d8d7556edba9a1629ff15cb1b35
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-12 22:56:58 -0400

    Update NumberTypes.jl

--- src/NumberTypes.jl
+++ src/NumberTypes.jl
@@ -1,14 +1,23 @@
 module NumberTypes
 
-export SysFloat, SysSigned, SysUnsigned, SysInteger
+export SysFloat, SysSInt, SysUInt,
+       SysNum128, SysNum64, SysNum32, SysNum16, SysNum8
 
-const SysSInt  = Union{ Int32,     Int64 } 
-const SysUInt  = Union{ UInt32,   UInt64 } 
-const SysFloat = Union{ Float32, Float64 } 
+const SysSigned   = Union{   Int64,   Int32 } 
+const SysUnsigned = Union{  UInt64,  UInt32 } 
+const SysFloat    = Union{ Float64, Float32 } 
 
-const SysFloat = Union{ Float32, Float64 } 
+const SystemFloat    = Union{ Float64, Float32, Float16 } 
+const SystemSigned   = Union{ Int128,   Int64,   Int32,  Int16,  Int8 } 
+const SystemUnsigned = Union{ UInt128, UInt64,  UInt32, UInt16, UInt8 } 
 
 
-SystemNumber64, SystemNumber32 SystemSigned, SystemUnsigned, System32, System64
+
+
+const SystemNum128 = Union{ Int128, UInt128 } 
+const SysNum64  = Union{ Int65, UInt65, Float64 } 
+const SysNum32  = Union{ Int32, UInt32, Float32 } 
+const SysNum16  = Union{ Int16, UInt16, Float16 } 
+const SystemNum8   = Union{ Int8, UInt8 } 
 
 end # module
