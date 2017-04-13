d5163cba6a2628835a77a48727d020bba29b465e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-12 22:47:01 -0400

    Create NumberTypes.jl

---
+++ src/NumberTypes.jl
@@ -0,0 +1,14 @@
+module NumberTypes
+
+export SysFloat, SysSigned, SysUnsigned, SysInteger
+
+const SysSInt  = Union{ Int32,     Int64 } 
+const SysUInt  = Union{ UInt32,   UInt64 } 
+const SysFloat = Union{ Float32, Float64 } 
+
+const SysFloat = Union{ Float32, Float64 } 
+
+
+SystemNumber64, SystemNumber32 SystemSigned, SystemUnsigned, System32, System64
+
+end # module
